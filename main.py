51cbcab9805b5d558da5ec249ee5b9b0b042988a
ZofiaLor <zosia984@gmail.com>
2025-12-29 18:54:36 +0100

    calculate clustering accuracy

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fuzzy_cmeans import FuzzyCMeans
 import os
 import time
 import skfuzzy as fuzz
+import pandas
 
 # Constants
 seed = 42
@@ -76,6 +77,54 @@ class DataEntry(object):
         print(np.mean(self.strict_number_times))
         for n in self.granules_number:
             print(np.mean(self.fuzzy_number_times[n]))
+
+    def calculate_accuracy(self, fcm, hc, nonfuzzy_labels):
+        granule_membership = []
+        granule_member_labels = []
+        for sample in fcm.U_:
+            membership = int(np.argmax(sample))
+            granule_membership.append(membership)
+            granule_member_labels.append(int(hc.labels[membership]))
+
+        l_matrix = np.zeros(shape=(self.clusters_number, self.clusters_number))
+        for d in range(len(granule_member_labels)):
+            l_matrix[nonfuzzy_labels[d], granule_member_labels[d]] = l_matrix[nonfuzzy_labels[d], granule_member_labels[
+                d]] + 1
+
+        for i in range(self.clusters_number - 1):
+            (row, col) = np.unravel_index(np.argmax(l_matrix[i:, i:]), (self.clusters_number - i, self.clusters_number - i))
+            row = row + i
+            col = col + i
+            l_matrix[[i, row], :] = l_matrix[[row, i], :]
+            l_matrix[:, [i, col]] = l_matrix[:, [col, i]]
+
+        sum_diagonal = 0
+        for i in range(self.clusters_number):
+            sum_diagonal = sum_diagonal + l_matrix[i, i]
+        return sum_diagonal / len(granule_member_labels)
+
+    def measure_accuracy(self):
+        result_list = []
+        ac = sklearn.cluster.AgglomerativeClustering(n_clusters=self.clusters_number, linkage='single')
+        ac.fit(self.data)
+        for n in self.granules_number:
+            fcm = FuzzyCMeans(n_clusters=n, random_state=seed, max_iter=n_iterations)
+            hc = HierarchicalClustering(n_clusters=self.clusters_number)
+            granules = []
+
+            fcm.fit(self.data)
+            fuzziness = calculate_fcm_variance(fcm)
+            for i in range(n):
+                granules.append(Granule(fcm.cluster_centers_[i], fuzziness[i]))
+            for relation_type in ['t', 'e', 'g']:
+                for k100 in range(5, 100, 5):
+                    k = k100 / 100
+                    print("n " + str(n) + " type " + relation_type + " ksi " + str(k))
+                    hc.fuzzy_fit(granules, k, relation_type)
+                    acc = self.calculate_accuracy(fcm, hc, ac.labels_)
+                    results = {"data size": self.length, "granules number": n, "relation type": relation_type, "ksi": k, "accuracy": acc}
+                    result_list.append(results)
+        return pandas.DataFrame(result_list)
 
     def fit_plot_fuzzy_labels(self, relation_type):
         print(self.name)
@@ -147,8 +196,8 @@ names = []
 num_of_clusters = {"blobs": 3, "circles": 2, "corners": 4, "crescents": 2, "laguna": 3, "spheres": 2}
 names_roots = ["blobs", "circles", "corners", "crescents", "laguna", "spheres"]
 # blobs10000 -> 7 minutes of clustering
-# data_size_presets = [1000, 2000, 5000, 10000, 20000, 30000, 40000, 50000]
-data_size_presets = [1000, 2000, 5000]
+data_size_presets = [1000, 2000, 5000, 10000, 20000, 30000, 40000, 50000]
+# data_size_presets = [1000, 2000, 5000]
 
 for folder in os.scandir("dane"):
     for file in os.scandir(folder.path):
@@ -165,6 +214,16 @@ for folder in os.scandir("dane"):
                     break
 
 # fullData['spheres2000'].fit_plot_fuzzy_labels('t')
+# fullData['corners1000'].measure_accuracy()
+for root in names_roots:
+    accuracy_results = pandas.DataFrame()
+    for data_size in data_size_presets:
+        print(root + str(data_size))
+        result = fullData[root + str(data_size)].measure_accuracy()
+        accuracy_results = pandas.concat([accuracy_results, result], ignore_index=True)
+    accuracy_results.to_csv("wyniki/" + root + "_accuracy.csv")
+
+
 
 # data = fullData['blobs1000'].data
 # cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(np.transpose(data), 100, 1.2, error=1e-4, maxiter=150, init=None)
